b44f807144dd2917a095a847cf50c04a4aefe472
Jennifer_Liu <thejenniferliu@gmail.com>
2022-12-06 08:09:17 -0700

    this was very hard

--- day1.py
+++ day1.py
@@ -10,6 +10,23 @@ while n**3 < 1000:
 
 #get first prime numbers up to 100
 #only divisible by 1, and divisible by itself. 
+def is_prime(number):
+    #take in a number and determine if its prime
+    for i in range(2, number):
+        #iterate through numbers greater than 1, up until input number
+        if number % i == 0:
+            #evaluate modulo to see if number is divisible by i
+            return False
+            #return false if any divisor is whole
+    return True
+    #return True if no numbers in range divide wholly
+
+def print_primes():
+    for i in range(1, 101):
+        if is_prime(i):
+            print(i)
+
+print(print_primes())
 
 
 #Take in a users input for their age, if they are younger than 18 print kids, if they're 18 to 65 print adults, else print seniors
